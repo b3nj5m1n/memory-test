6eed3949aad8653de111c3d653820bc5a5ec828c
b3nj4m1n <b3nj4m1n@gmx.net>
2020-04-12 16:06:09 +0200

    First usable version

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
+// What constant is used, default is pi
+var use = "π";
+
 // Variable for the constant
 var constant = "";
+// Variable for correctly recalled numbers
+var correct_count = 0;
 
 $(document).ready(function () {
-    recall_target = "π";
+    setup();
+});
+
+function setup() {
+    // Remove all the inputs
+    $('.number-fields-input').each(function (i, obj) {
+        $(this).remove();
+    });
+    recall_target = use;
     $(".recall-target").text(recall_target);
     // Get constant from json
-    constant = JSON.parse(e);
+    if (use == "π") {
+        constant = JSON.parse(pi);
+    } else if (use == "φ") {
+        constant = JSON.parse(phi);
+    } else if (use == "e") {
+        constant = JSON.parse(e);
+    }
+    
     // Add the first digit (Not a decimal)
     add_input();
     // Add a second input field
     add_input();
-});
+
+    correct_count = 0;
+    $("#correct-recall-indicator").text("")
+}
 
 // This first makes all previous inputs read only and then adds a new input
 function add_input() {
@@ -22,11 +45,11 @@ function add_input() {
     // If this is the first one added
     if (count == -1) {
         // Add an input with the number before the decimal points and a dot
-        input = $('<input value="' + constant[0]["start"] + "." + '" id="number-fields-input-num-' + count + '" oninput="add_input();" class="number-fields-input" type="text" maxlength="2">')
+        input = $('<input value="' + constant[0]["start"] + "." + '" style="display: inline;" id="number-fields-input-num-' + count + '" oninput="add_input();" class="number-fields-input form-control" type="text" maxlength="2">')
         $("#number-fields").append(input);
     } else {
         // Add an empty input field
-        input = $('<input id="number-fields-input-num-' + count + '" oninput="add_input();" class="number-fields-input" type="text" maxlength="1">')
+        input = $('<input style="display: inline;" id="number-fields-input-num-' + count + '" oninput="add_input();" class="number-fields-input form-control" type="text" maxlength="1">')
         $("#number-fields").append(input);
     }
     // If a digit has been entered manually in the last field
@@ -46,8 +69,21 @@ function check_input(index) {
     if (correct_digit == typed_digit) {
         console.log("Correct! You entered: " + typed_digit + ", the correct digit at that position is: " + correct_digit + "; index: " + (index-1).toString());
         input.css("background-color", "green");
+        correct_count += 1;
+        $("#correct-recall-indicator").text("You recalled " + correct_count + " digits correcty.")
     } else {
         console.log("Incorrect! You entered: " + typed_digit + ", the correct digit at that position is: " + correct_digit + "; index: " + (index-1).toString());
         input.css("background-color", "red");
     }
+}
+
+// Functions to switch use
+function switch_constant(param, sender) {
+    use = param;
+    setup();
+    // Remove active from all entrys in header
+    $('.active').each(function (i, obj) {
+        $(this).removeClass("active");
+    });
+    $(sender).addClass("active");
 }
