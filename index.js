5460513ec4c77c8c2a4540f9e272816850fad11d
b3nj4m1n <b3nj4m1n@gmx.net>
2020-04-13 15:41:27 +0200

    Changed way of checking

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var constant = "";
 var correct_count = 0;
 
 $(document).ready(function () {
-    setup();
+    // setup();
 });
 
 function setup() {
+    num_of_digits = $("#recall-digits-count").val();
     // Remove all the inputs
     $('.number-fields-input').each(function (i, obj) {
         $(this).remove();
@@ -25,21 +26,40 @@ function setup() {
     } else if (use == "e") {
         constant = JSON.parse(e);
     }
-    
+
     // Add the first digit (Not a decimal)
     add_input();
-    // Add a second input field
-    add_input();
+    // // Add a second input field
+    // add_input();
+    // Add inputs for all of the digits
+    while (add_input() < num_of_digits-1) { }
+
+    // Show submit button
+    $("#submit-numbers-btn").removeClass("hidden");
 
     correct_count = 0;
     $("#correct-recall-indicator").text("")
+}
+
+function submit() {
+    count = -1
+    // Make all the inputs read only
+    $('.number-fields-input').each(function (i, obj) {
+        $(this).prop("readonly", true);
+        if (count > -1) {
+            check_input(count+1);   
+        }
+        count += 1;
+    });
+    // Check all the inputs
+
 }
 
 // This first makes all previous inputs read only and then adds a new input
 function add_input() {
     var count = -1
     $('.number-fields-input').each(function (i, obj) {
-        $(this).prop("readonly", true);
+        // $(this).prop("readonly", true);
         count = i;
     });
     // If this is the first one added
@@ -49,31 +69,49 @@ function add_input() {
         $("#number-fields").append(input);
     } else {
         // Add an empty input field
-        input = $('<input id="number-fields-input-num-' + count + '" style="background-color: #2F363F;" oninput="add_input();" class="number-fields-input form-control" type="text" maxlength="1">')
+        input = $('<input id="number-fields-input-num-' + count + '" style="background-color: #2F363F;" oninput="focus_next(this); //add_input();" class="number-fields-input form-control" type="text" maxlength="1">')
         $("#number-fields").append(input);
     }
+    return count;
     // If a digit has been entered manually in the last field
-    if (count > 0) {
-        // Check the last input field
-        check_input(count)
-    }
+    // if (count > 0) {
+    //     // Check the last input field
+    //     check_input(count)
+    // }
 
-    // Focus the newest input field
-    $(".number-fields-input").last().focus();
+    // // Focus the newest input field
+    // $(".number-fields-input").last().focus();
+}
+
+function focus_next(sender) {
+    var count = -1
+    var now = false;
+    $('.number-fields-input').each(function (i, obj) {
+        // $(this).prop("readonly", true);
+
+        if (now) {
+            $(this).focus();
+            now = false;
+        }
+        if ($(sender).attr('id') == $(this).attr('id')) {
+            now = true;
+        }
+        count = i;
+    });
 }
 
 function check_input(index) {
     input = $(".number-fields-input").eq(index);
-    correct_digit = constant[index][(index-1).toString()]
+    correct_digit = constant[index][(index - 1).toString()]
     typed_digit = input.val();
     if (correct_digit == typed_digit) {
-        console.log("Correct! You entered: " + typed_digit + ", the correct digit at that position is: " + correct_digit + "; index: " + (index-1).toString());
+        console.log("Correct! You entered: " + typed_digit + ", the correct digit at that position is: " + correct_digit + "; index: " + (index - 1).toString());
         input.css("color", "#43BE31");
         input.css("border-color", "#43BE31");
         correct_count += 1;
         $("#correct-recall-indicator").text("You recalled " + correct_count + " digits correcty.")
     } else {
-        console.log("Incorrect! You entered: " + typed_digit + ", the correct digit at that position is: " + correct_digit + "; index: " + (index-1).toString());
+        console.log("Incorrect! You entered: " + typed_digit + ", the correct digit at that position is: " + correct_digit + "; index: " + (index - 1).toString());
         input.css("color", "#D63031");
         input.css("border-color", "#D63031");
     }
